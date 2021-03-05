7d22b4edddbcf99ee7452b0f3b8b55b1faf4ba52
Miguel Angel Lopez Arcos <migue300995@gmail.com>
2021-03-05 01:05:23 -0600

    Configure controller to use SolarSystemService

--- SolarSystem.Web/Controllers/WeatherForecastController.cs
+++ SolarSystem.Web/Controllers/WeatherForecastController.cs
@@ -1,34 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using SolarSystem.Domain.Models;
+using SolarSystem.Domain.SolarSystemService;
 
 namespace SolarSystem.Web.Controllers {
 	[ApiController]
-	[Route("[controller]")]
 	public class WeatherForecastController : ControllerBase {
-		private static readonly string[] Summaries = new[]
-		{
-			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-		};
+		private readonly ISolarSystemService solarSystemService;
 
-		private readonly ILogger<WeatherForecastController> _logger;
-
-		public WeatherForecastController(ILogger<WeatherForecastController> logger) {
-			_logger = logger;
+		public WeatherForecastController(ISolarSystemService solarSystemService) {
+			this.solarSystemService = solarSystemService;
 		}
 
-		[HttpGet]
-		public IEnumerable<WeatherForecast> Get() {
-			var rng = new Random();
-			return Enumerable.Range(1, 5).Select(index => new WeatherForecast {
-				Date = DateTime.Now.AddDays(index),
-				TemperatureC = rng.Next(-20, 55),
-				Summary = Summaries[rng.Next(Summaries.Length)]
-			})
-			.ToArray();
+		[HttpGet("clima")]
+		public string GetWeatherForDay([FromQuery] int? day) {
+			return solarSystemService.GetWeatherForDay(day ?? 0).ToString();
+		}
+
+		[HttpGet("condiciones")]
+		public MeteorologicalConditions GetConditionsForDay([FromQuery] int? day) {
+			return solarSystemService.GetConditionsForDay(day ?? 0);
 		}
 	}
 }
