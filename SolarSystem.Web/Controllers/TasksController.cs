f83405ea3cd0eff2e94ea1f5f524109ee669f110
Miguel Angel Lopez Arcos <migue300995@gmail.com>
2021-03-07 01:39:03 -0600

    Create Tasks controller

---
+++ SolarSystem.Web/Controllers/TasksController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SolarSystem.Domain.Services.Conditions10YearsService;
+using System;
+using System.Threading.Tasks;
+
+namespace SolarSystem.Web.Controllers {
+	public class TasksController : BasicController {
+		private readonly IJobCalculateConditions jobCalculateConditions;
+
+		public TasksController(IJobCalculateConditions jobCalculateConditions) {
+			this.jobCalculateConditions = jobCalculateConditions;
+		}
+
+		[HttpGet("task/populate")]
+		public async Task<IActionResult> PopulateDatabase([FromQuery] int? days) {
+			try{
+				await jobCalculateConditions.UploadConditionsToDatabase(days ?? 365);
+				return Ok(new {
+					Message = "Se cargaron los datos"
+				});
+			} catch(Exception e){
+				return DefaultCatch(e);
+			}
+		}
+	}
+}
