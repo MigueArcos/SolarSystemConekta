9611302f114ec85b8e0847381399dce0bce791ea
Miguel Angel Lopez Arcos <migue300995@gmail.com>
2021-03-07 01:36:05 -0600

    Add custom errors handling logic

---
+++ SolarSystem.Domain/Models/Errors.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SolarSystem.Domain.Models {
+	public struct ErrorMessages {
+		public const string UnknownError = "Error desconocido";
+
+		public const string EntityNotFound = "No se encontro información";
+
+		public const string WeatherNotFoundForDay = "No se encontraron condiciones climáticas para este día";
+	}
+
+	public class ErrorDetail {
+		public string Message { get; set; }
+		// public string Description { get; set; }
+	}
+
+	public class ErrorStatusCode : Exception {
+		public ErrorDetail Detail { get; set; }
+		public int HttpStatusCode { get; set; }
+		public ErrorStatusCode(int httpStatusCode, string errorMessage) {
+			Detail = new ErrorDetail {
+				Message = errorMessage
+			};
+			HttpStatusCode = httpStatusCode;
+		}
+		public static readonly ErrorStatusCode UnknownError = new ErrorStatusCode(500, ErrorMessages.UnknownError);
+
+		public static readonly ErrorStatusCode EntityNotFound = new ErrorStatusCode(404, ErrorMessages.EntityNotFound);
+
+		public static readonly ErrorStatusCode WeatherNotFoundForDay = new ErrorStatusCode(404, ErrorMessages.WeatherNotFoundForDay);
+	}
+}
---
+++ SolarSystem.Web/Controllers/BasicController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SolarSystem.Domain.Models;
+using System;
+
+namespace SolarSystem.Web.Controllers {
+	public class BasicController : Controller {
+		protected ObjectResult DefaultCatch(Exception exception) {
+			ErrorStatusCode error = null;
+			if (exception is ErrorStatusCode) {
+				error = exception as ErrorStatusCode;
+			}
+			else {
+				//We should never expose real exceptions, so we will catch all unknown exceptions (DatabaseErrors, Null Errors, Index errors, etc...) and rethrow an UnknownError after log
+				Console.WriteLine(exception);
+				error = ErrorStatusCode.UnknownError;
+			}
+			return new ObjectResult(error.Detail) {
+				StatusCode = error.HttpStatusCode
+			};
+		}
+	}
+}
